af9dbba2b8de2919b6a039e915b2b9354fbaaf6e
GraceWebpro <102426261+GraceWebpro@users.noreply.github.com>
2025-03-23 14:35:59 +0100

    your commit message

--- src/components/home/Homepage.js
+++ src/components/home/Homepage.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Banner from '../banner/Banner'
-import Skills from '../skills/Skills'
+//import Skills from '../skills/Skills'
+import Projects from '../projects/Projects';
+import Slider from '../skills/Skills';
 
 const Homepage = () => {
     return (
         <div style={{ marginTop: '60px' }}>
             
             <Banner />
-            <Skills />
-            <div style={{ textAlign: 'center' }}>
-                <h1>homepage</h1>
-            </div>
+            <Projects />
+            <Slider />
         </div>
     )
 };
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./Skills.css";
+import dev1 from '../../assets/dev2.png';
+import dev2 from '../../assets/dev2.png';
+import dev3 from '../../assets/dev2.png';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 4 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 3,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 464 },
+    items: 2,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+const sliderImageUrl = [
+  //First image url
+  {
+    url: dev1,
+    title: "Web Development"
+  },
+  {
+    url: dev2,
+    title: "UI/UX Design"
+  },
+  //Second image url
+  {
+    url: dev3,
+    title: "Logo Design"
+  },
+  //Third image url
+  {
+    url: dev1,
+    title: "Web Development"
+  },
+
+  //Fourth image url
+
+  {
+    url: dev2,
+    title: "Social Media"
+  }
+];
+const Slider = () => {
+  return (
+    <div className="cont">
+      <h2>Skills</h2>
+      <p>this is the skills section of this portfolio</p>
+
+      <div className="parent">
+        <Carousel
+          responsive={responsive}
+          autoPlay={true}
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          infinite={true}
+          partialVisible={false}
+          dotListClass="custom-dot-list-style"
+        >
+          {sliderImageUrl.map((imageUrl, index) => {
+            return (
+              <div className="slider" key={index}>
+                <img src={imageUrl.url} alt="movie" />
+                <p>{imageUrl.title}</p>
+              </div>
+            );
+          })}
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+export default Slider;
