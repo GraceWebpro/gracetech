85c13faf65f39e7c6a3f27645119e082fef14e4f
GraceWebpro <102426261+GraceWebpro@users.noreply.github.com>
2026-06-19 13:46:39 +0100

    changes

--- api/verify-payment.js
+++ api/verify-payment.js
@@ -1,9 +1,16 @@
 export default async function handler(req, res) {
+  console.log("BODY:", req.body); // 👈 add this
+
     if (req.method !== "POST") {
       return res.status(405).json({ message: "Method not allowed" });
     }
   
     const { transaction_id } = req.body;
+
+    if (!transaction_id) {
+      console.log("❌ Missing transaction_id");
+      return res.status(400).json({ error: "No transaction_id provided" });
+    }
   
     try {
       const response = await fetch(
@@ -20,7 +27,8 @@ export default async function handler(req, res) {
 
       console.log("FLW RESPONSE:", data); // 👈 ADD THIS
 
-  
+      console.log("KEY:", process.env.FLW_SECRET_KEY);
+      
       if (
         data.status === "success" &&
         (data.data.status === "successful" || data.data.status === "completed")
@@ -32,9 +40,11 @@ export default async function handler(req, res) {
       } else {
         return res.status(400).json({
           success: false,
+          message: data,
         });
       }
     } catch (error) {
+      console.error("ERROR:", error);
       return res.status(500).json({ error: "Verification failed" });
     }
   }
