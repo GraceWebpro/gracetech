561756e78aacc31ab48794adcfbe3ffdfcfa894e
GraceWebpro <102426261+GraceWebpro@users.noreply.github.com>
2026-07-01 12:15:38 +0100

    create

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals.js';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './config/AuthProvider';
 import smoothscroll from 'smoothscroll-polyfill';
