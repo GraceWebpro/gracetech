436eb0381bd66710d1d92ba1a22c7f3f4309c857
GraceWebpro <102426261+GraceWebpro@users.noreply.github.com>
2026-07-11 09:54:57 +0100

    track

--- src/config/analytics.js
+++ src/config/analytics.js
@@ -4,6 +4,12 @@ const MEASUREMENT_ID = "G-B0GP9WJV7V";
 
 export const initGA = () => {
   ReactGA.initialize(MEASUREMENT_ID);
+
+  window.gtag("config", MEASUREMENT_ID, {
+    debug_mode: true,
+  });
+
+  console.log("GA initialized");
 };
 
 export const logPageView = (path) => {
@@ -14,5 +20,5 @@ export const logPageView = (path) => {
 };
 
 export const event = (name, params = {}) => {
-    ReactGA.event(name, params);
+  ReactGA.event(name, params);
 };
